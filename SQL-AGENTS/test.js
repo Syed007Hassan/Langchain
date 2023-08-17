7be192c55b1446dad64dbb76f39834e420cacc9d
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-17 23:18:47 +0500

    axios request

--- SQL-AGENTS/test.js
+++ SQL-AGENTS/test.js
@@ -2,11 +2,15 @@ import axios from "axios";
 
 const prompt = "Get all the employees who were hired after 2005?";
 
-axios
-  .get(`http://localhost:5000/query?prompt=${encodeURIComponent(prompt)}`)
-  .then((response) => {
-    console.log(response.data);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+try {
+  const response = await axios.get(
+    `http://localhost:5000/query?prompt=${encodeURIComponent(prompt)}`
+  );
+
+  const data = response.data;
+
+  console.log(data);
+
+} catch (error) {
+  console.log(error);
+}
