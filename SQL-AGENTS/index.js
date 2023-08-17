abeed83d301490cd3a906c4edc58c8cd25840865
Syed Muhammed Hassan Ali <syed007hassan@gmail.com>
2023-08-17 21:45:53 +0500

    api working

--- SQL-AGENTS/index.js
+++ SQL-AGENTS/index.js
@@ -14,55 +14,55 @@ console.log("prompt: " + prompt);
 
   configDotenv();
 
-  // const datasource = new DataSource({
-  //   type: "sqlite",
-  //   database: "./data/northwind.db",
-  // });
+  const datasource = new DataSource({
+    type: "sqlite",
+    database: "./data/northwind.db",
+  });
 
-  // const db = await SqlDatabase.fromDataSourceParams({
-  //   appDataSource: datasource,
-  // });
+  const db = await SqlDatabase.fromDataSourceParams({
+    appDataSource: datasource,
+  });
 
-  // const toolkit = new SqlToolkit(db);
-  // const model = new OpenAI({
-  //   temperature: 0,
-  // });
+  const toolkit = new SqlToolkit(db);
+  const model = new OpenAI({
+    temperature: 0,
+  });
 
-  // const executor = createSqlAgent(model, toolkit);
+  const executor = createSqlAgent(model, toolkit);
 
-  // let response = {
-  //   prompt: prompt,
-  //   output: "",
-  //   intermediateSteps: [],
-  //   error: "",
-  // };
+  let response = {
+    prompt: prompt,
+    output: "",
+    intermediateSteps: [],
+    error: "",
+  };
 
-  // try {
-  //   const result = await executor.call({ input: prompt });
+  try {
+    const result = await executor.call({ input: prompt });
 
-  //   result.intermediateSteps.forEach((step) => {
-  //     if (step.action.tool === "query-sql") {
-  //       response.prompt = prompt;
-  //       response.output = step.observation;
-  //       response.intermediateSteps = result.intermediateSteps;
-  //     }
-  //   });
+    result.intermediateSteps.forEach((step) => {
+      if (step.action.tool === "query-sql") {
+        response.prompt = prompt;
+        response.output = step.observation;
+        response.intermediateSteps = result.intermediateSteps;
+      }
+    });
 
-  //   console.log(
-  //     `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
-  //   );
+    console.log(
+      `Intermediate steps ${JSON.stringify(result.intermediateSteps, null, 2)}`
+    );
 
-  //   res.json(response);
-  // } catch (e) {
-  //   console.log(e + " " + "my error message");
-  //   response.error = "Server error. Try again with a different prompt.";
+    res.json(response);
+  } catch (e) {
+    console.log(e + " " + "my error message");
+    response.error = "Server error. Try again with a different prompt.";
 
-  //   res.status(500).json(response);
-  // }
+    res.status(500).json(response);
+  }
 
-  // await datasource.destroy();
+  await datasource.destroy();
 });
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
 });
--- SQL-AGENTS/test.js
+++ SQL-AGENTS/test.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const prompt = "Get all the employees who were hired after 2005?";
 
 axios
-  .get(`http://localhost:3000/query?prompt=${encodeURIComponent(prompt)}`)
+  .get(`http://localhost:5000/query?prompt=${encodeURIComponent(prompt)}`)
   .then((response) => {
     console.log(response.data);
   })
